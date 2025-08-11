fd40bc26485e0fe913e970ddb16b2762c649a7d9
DatTran0509 <dat881070@gmail.com>
2025-08-11 15:52:26 +0700

    update readme and img

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -6,7 +6,7 @@ import { useContext } from 'react';
 import { AdminContext } from './context/AdminContext.jsx';
 import Navbav from './components/Navbav.jsx';
 import Sidebar from './components/Sidebar.jsx';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './pages/Admin/Dashboard.jsx';
 import AllAppointments from './pages/Admin/AllAppointments.jsx';
 import AddDoctor from './pages/Admin/AddDoctor.jsx';
@@ -17,32 +17,42 @@ import DoctorAppointments from './pages/Doctor/DoctorAppointments.jsx';
 import DoctorProfile from './pages/Doctor/DoctorProfile.jsx';
 
 const App = () => {
-const {aToken} = useContext(AdminContext)
-const {dToken} = useContext(DoctorContext)
+  const { aToken } = useContext(AdminContext)
+  const { dToken } = useContext(DoctorContext)
+  
+  // Tạo component để redirect về dashboard phù hợp
+  const HomePage = () => {
+    if (aToken) {
+      return <Navigate to="/admin-dashboard" replace />
+    } else if (dToken) {
+      return <Navigate to="/doctor-dashboard" replace />
+    }
+  }
+
   return aToken || dToken ? (
     <div className='bg-[#F8F9FD]'>
-      <ToastContainer/>
-      <Navbav/>
-      <div className='flex items-start gap-4'> 
-        <Sidebar/>
+      <ToastContainer />
+      <Navbav />
+      <div className='flex items-start gap-4'>
+        <Sidebar />
         <Routes>
-          <Route path='/' element={<Dashboard/>}/>
-          <Route path='/admin-dashboard' element={<Dashboard/>}/>
-          <Route path='/all-appointments' element={<AllAppointments/>}/>
-          <Route path='/add-doctor' element={<AddDoctor/>}/>
-          <Route path='/doctor-list' element={<DoctorsList/>}/>    
+          <Route path='/' element={<HomePage />} />
+          <Route path='/admin-dashboard' element={<Dashboard />} />
+          <Route path='/all-appointments' element={<AllAppointments />} />
+          <Route path='/add-doctor' element={<AddDoctor />} />
+          <Route path='/doctor-list' element={<DoctorsList />} />
+          
+          <Route path='/doctor-dashboard' element={<DoctorDashboard />} />
+          <Route path='/doctor-appointments' element={<DoctorAppointments />} />
+          <Route path='/doctor-profile' element={<DoctorProfile />} />
 
-        <Route path='/doctor-dashboard' element={<DoctorDashboard/>}/>
-        <Route path='/doctor-appointments' element={<DoctorAppointments/>}/>
-        <Route path='/doctor-profile' element={<DoctorProfile/>}/>
-             
         </Routes>
       </div>
     </div>
-  ):(
+  ) : (
     <>
-      <Login/>
-      <ToastContainer/>
+      <Login />
+      <ToastContainer />
     </>
   )
 }
